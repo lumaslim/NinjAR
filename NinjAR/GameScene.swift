ed8df577e5c4a9ece5e40a1a29729d262fed4326
SLimma <30815731+slimlime@users.noreply.github.com>
2018-12-20 15:43:34 +1000

    🔥Removing lame tutorial code

--- NinjAR/GameScene.swift
+++ NinjAR/GameScene.swift
@@ -51,24 +51,7 @@ class GameScene: SKScene {
             CGPoint(size.width, 0), // 1000,0 scaled to 700,24.5 ? with aspectfill and gamescene on ipadpro12.9 res
             CGPoint(size.width, size.height)
         ]
-        let monsters = [makeMonster(), makeMonster(), makeMonster(), makeMonster()]
 
-        //        let spawnXRightWall = size.width - monster.size.width
-        //        let spawnYRandom = random(min: monster.size.height, max: size.height)
-        for (index, monster) in monsters.enumerated() {
-            
-            
-            // Spawn
-            addChild(monster)
-            positionMobNodeInParent(monster, at: spawnPoints[index])
-            // Physics and collision
-            guard let _ = setupPhysics(onMonster: monster) else { return }
-            
-            // Movement
-            monster.run(getMonsterMoveChainDespawnSequence(), withKey: "MoveDespawn")
-            
-        }
-        
         print("touchesBegan:: end")
         
         
@@ -78,11 +61,7 @@ class GameScene: SKScene {
         guard let firstTouch = touches.first else { return }
         let touchLocationInScene = firstTouch.location(in: self)
         
-        let projectile = shootProjectile(at: touchLocationInScene)
-        print("GameScene:: touchesEnded end")
         
-        // Physics and detection
-        guard let _: SKPhysicsBody = setupPhysics(onProjectile: projectile) else { return }
         
     }
 }
@@ -99,145 +78,15 @@ extension CGPoint {
         self.init(x: convenientX, y: convenientY)
     }
 }
-// - MARK: Monstera
-extension GameScene {
-    // lol
-    func spawn(monster: SKSpriteNode, onParent gameScene: GameScene) {
-        gameScene.addChild(monster)
-    }
-    func makeMonster() -> SKSpriteNode {
-        return SKSpriteNode(imageNamed: "monster")
-    }
-    
-    // welp superfluous
-    func positionMobNodeInParent(_ monster: SKSpriteNode, at point: CGPoint) {
-        // Does position use top-left origin for relative coordinates?
-        //        let spawnXRightWall = size.width - monster.size.width
-        //        let spawnYRandom = random(min: monster.size.height, max: size.height)
-        
-        monster.position = point
-    }
-    func setMonsterMove(monster: SKSpriteNode, move moveAction: SKAction) {
-        monster.run(moveAction, withKey: "randomMove")
-    }
-    func getMonsterMoveChainDespawnSequence() -> SKAction {
-        // Sequence of multiple actions is also an SKAction
-        return SKAction.sequence([
-            getMonsterMoveAction(),
-            getDespawnMonsterAction()
-        ])
-    }
-    func getDespawnMonsterAction() -> SKAction {
-        // Removes node from the parent. e.g. SCNNode mob that runs this action.
-        return SKAction.removeFromParent()
-    }
-    func getMonsterMoveAction() -> SKAction {
-        print("movin", size)
-        // Strange behaviour depending on portrait landscape. landscape seems to work as expected.
-        // Monster movement
-        let moveDurationRandom = TimeInterval(random(min: 1.0, max: 6.5)) // feels redundant
-        let moveDestination = CGPoint(x: size.width / 2, y: size.height / 2) // 768,1024 ends up near the tap point of 470,0 ... Strange
-        let moveAction = SKAction.move(to: moveDestination, duration: moveDurationRandom)
-        
-        return moveAction
-    }
-}
-// Generalise the commonalities between monster player and projectiles. Mob and projectiles?
-// - MARK: Projectiles
-extension GameScene {
-    func shootProjectile(at proposedTarget: CGPoint) -> SKSpriteNode {
-        let projectile = SKSpriteNode(imageNamed: "projectile")
-        projectile.position = player.position
-        let directionalOffset = proposedTarget - projectile.position
-        
-        // Set up vectors for directional shooting and movement action.
-        let direction = directionalOffset.normalized()
-        let shootDistance: CGPoint = direction * size.width // arbitrary distance to go the distance... and hit the target
-        let destination: CGPoint = shootDistance + projectile.position
-        
-        // Attach the projectile to view the spawning shoot effect)
-        addChild(projectile)
-        
-        // Shoot motion
-        let projectileArbitrarySpeedRatioSecs = TimeInterval(2.0)
-        let projectileMoveAction = SKAction.move(to: destination, duration: projectileArbitrarySpeedRatioSecs)
-        let projectileDone = SKAction.removeFromParent()
-        let projectileShootActionsDoneChain = SKAction.sequence([projectileMoveAction, projectileDone])
 
-        projectile.run(projectileShootActionsDoneChain, withKey: "ShootDone")
-        
-        return projectile
-    }
-//    func projectileMoveActionsDoneChain() -> SKAction {
-//
-//    }
-}
-
-//
-
-// - MARK: Mob Physics and collision detection
-extension GameScene {
-    func setupPhysics(onMonster monster: SKSpriteNode) -> SKPhysicsBody? {
-        monster.physicsBody = SKPhysicsBody(rectangleOf: monster.size)
-        
-        guard let monsterPhysics = monster.physicsBody else { return nil }
-        monsterPhysics.isDynamic = true
-        monsterPhysics.categoryBitMask = PhysicsCategory.monster
-        // Intersection notification on AND the bit categories.
-        monsterPhysics.contactTestBitMask = PhysicsCategory.projectile
-        // Bouncy contact
-        monsterPhysics.collisionBitMask = PhysicsCategory.none
-        
-        return monsterPhysics // returns the physicsbody configured.
-    }
-    
-    func setupPhysics(onProjectile projectile: SKSpriteNode) -> SKPhysicsBody? {
-        let projectilePhysics = SKPhysicsBody(circleOfRadius: projectile.size.width)
-        
-        projectilePhysics.isDynamic = true
-        projectilePhysics.categoryBitMask = PhysicsCategory.projectile
-        projectilePhysics.contactTestBitMask = PhysicsCategory.monster
-        
-        // seems redundant to test contact with both monster->projectile and projectile->monster
-        // rip tutorial games programming where is good software design patterns complexity
-        projectilePhysics.collisionBitMask = PhysicsCategory.none
-        // Precision important for fast moving phys clipping detection issues
-        projectilePhysics.usesPreciseCollisionDetection = true
-        
-        // oops
-        projectile.physicsBody = projectilePhysics
-        
-        return projectilePhysics
-    }
-    
-    func projectileDidCollide(projectile: SKNode, hit monster: SKNode) {
-        print("Pew pew hit")
-        projectile.removeFromParent()
-        monster.removeFromParent()
-    }
-}
 
 // - MARK: Collision detection contact
 extension GameScene: SKPhysicsContactDelegate {
     
     func didBegin(_ contact: SKPhysicsContact) {
         print("Pew pew contact", contact)
-        
-        // Samples identify the physics body using the nested property bit category...
-        // Sounds like a better use case for polymorphism subclass of phys body
         let bodyA = contact.bodyA
         let bodyB = contact.bodyB
-        // Maybe if categorising collision for only one instead of both monster and projectile
-        // Can have more standard behaviour body A vs B?
-        
-        // Monster was smallest bit. b01 = 1
-        // Projectile b10 = 2
-        let (monsterPhysics, projectilePhysics) = bodyA.categoryBitMask < bodyB.categoryBitMask ? (bodyA, bodyB) : (bodyB, bodyA)
-        guard let monster: SKNode = monsterPhysics.node else { print("monster ded"); return; }
-        guard let projectile: SKNode = projectilePhysics.node else { print("bullet borked"); return; }
-        
-        // Extra checks. seems dangerous
-        projectileDidCollide(projectile: projectile, hit: monster)
     }
 }
 
